7b437e3cc854da4a758ce55e78070cf93bffaffe
Gauthier GALON <gauthiergalon@pm.me>
2024-05-16 13:48:25 +0200

    WIP

--- module08/ex02/inc/MutantStack.hpp
+++ module08/ex02/inc/MutantStack.hpp
@@ -33,7 +33,11 @@ class MutantStack : public std::stack<T>
 			return (*this);
 		}
 
-		typedef typename std::deque<T>::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
 		iterator begin()
 		{
 			return (this->c.begin());
@@ -41,6 +45,36 @@ class MutantStack : public std::stack<T>
 		iterator end()
 		{
 			return (this->c.end());
+		}
+
+		const_iterator cbegin() const
+		{
+			return this->c.begin();
+		}
+
+		const_iterator cend() const
+		{
+			return this->c.end();
+		}
+
+		reverse_iterator rbegin()
+		{
+			return this->c.rbegin();
+		}
+
+		reverse_iterator rend()
+		{
+			return this->c.rend();
+		}
+
+		const_reverse_iterator crbegin() const
+		{
+			return this->c.rbegin();
+		}
+
+		const_reverse_iterator crend() const
+		{
+			return this->c.rend();
 		}
 };
 
