2a87dd8130b143b71c273304715bcc8a2767188f
Gauthier <gauthiergalon@pm.me>
2024-05-20 15:55:16 +0200

    WIP

--- module09/ex00/src/main.cpp
+++ module09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ggalon <ggalon@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 19:54:01 by ggalon            #+#    #+#             */
-/*   Updated: 2024/05/18 21:31:44 by ggalon           ###   ########.fr       */
+/*   Updated: 2024/05/20 15:52:10 by ggalon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,9 @@ void insertData(const std::string& line, std::map<std::string, double>& map)
 	map.insert(std::pair<std::string, double>(date, value));
 }
 
-int data()
+int data(std::map<std::string, double> &map)
 {
 	std::ifstream infile;
-	std::map<std::string, double> map;
 	std::string line;
 
 	infile.open("data.csv");
@@ -48,12 +47,74 @@ int data()
 		insertData(line, map);
 	}
 	infile.close();
+	return (0);
 }
 
-int exchange(const char *filepath)
+void readData(const std::string &line, std::map<std::string, double> &map)
+{
+	if (line == "date | value" || line.empty())
+		return ;
+	
+	if (line.length() < 14)
+	{
+		std::cerr << "Formatting error: Lengh" << std::endl;
+		return ;
+	}
+	if (line.substr(10, 3) != " | ")
+	{
+		std::cerr << "Formatting error: ' | '" << std::endl;
+		return ;
+	}
+	
+	std::string date(line.substr(0, 10));
+	for (size_t i = 0; i < date.length(); i++)
+	{
+		if ((i < 4 || i == 5 || i == 6 || i == 8 || i == 9) && !std::isdigit(date[i]))
+		{
+			std::cerr << "Formatting error: Date format" << std::endl;
+			return ;
+		}
+		if ((i == 4 || i == 7) && date[i] != '-')
+		{
+			std::cerr << "Formatting error: Date format" << std::endl;
+			return ;
+		}
+	}
+
+	double value;
+	std::stringstream ss(line.substr(13));
+	ss >> value;
+	if (value < 0 || value > 1000)
+	{
+		std::cerr << "Formatting error: Value" << std::endl;
+		return ;
+	}
+	int month = std::atoi(date.substr(5, 2).c_str());
+	int day = std::atoi(date.substr(8, 2).c_str());
+	if (month > 12 || day > 31 || month == 0 || day == 0)
+	{
+		std::cerr << "Formatting error: Date value" << std::endl;
+		return ;
+	}
+	
+	double total;
+	std::map<std::string, double>::iterator it;
+	it = map.lower_bound(date);
+	if (it == map.end())
+	{
+		it--;
+		total = it->second;
+	}
+	else
+	{
+		total = it->second * value;
+	}
+	std::cout << date << " => " << value << " = " << total << std::endl;
+}
+
+int exchange(const char *filepath, std::map<std::string, double> &map)
 {
 	std::ifstream infile;
-	std::map<std::string, double> map;
 	std::string line;
 
 	infile.open(filepath);
@@ -62,8 +123,12 @@ int exchange(const char *filepath)
 		error("Cannot open file");
 		return (1);
 	}
-	
+	while (getline(infile, line))
+	{
+		readData(line, map);
+	}
 	infile.close();
+	return (0);
 }
 
 int main(int argc, char const *argv[])
@@ -74,19 +139,24 @@ int main(int argc, char const *argv[])
 		error("Wrong argument number");
 		return (1);
 	}
-	if (data())
+
+	std::map<std::string, double> map;
+	if (data(map))
 	{
 		return (1);
 	}
-	if (exchange(argv[1]))
+	if (exchange(argv[1], map))
 	{
 		return (1);
 	}
 	
-
+	// int i;
 	// for (std::map<std::string, double>::iterator it = map.begin(); it != map.end(); ++it)
 	// {
 	// 	std::cout << it->first << " " << it->second << std::endl;
+	// 	i++;
+	// 	if (i == 250)
+	// 		break ;
 	// }
 	return (0);
 }
