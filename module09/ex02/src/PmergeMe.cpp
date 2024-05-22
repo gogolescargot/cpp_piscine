53ba2f8b1ab534359b5700d56ab8d690696886cd
Gauthier <gauthiergalon@pm.me>
2024-05-22 20:46:12 +0200

    WIP

--- module09/ex02/src/PmergeMe.cpp
+++ module09/ex02/src/PmergeMe.cpp
@@ -6,11 +6,24 @@
 /*   By: ggalon <ggalon@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:28:49 by ggalon            #+#    #+#             */
-/*   Updated: 2024/05/21 22:09:18 by ggalon           ###   ########.fr       */
+/*   Updated: 2024/05/22 20:44:19 by ggalon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+size_t jacobsthal(size_t x)
+{
+	if (x == 0)
+	{
+		return (0);
+	}
+	if (x == 1)
+	{
+		return (1);
+	}
+	return (jacobsthal(x - 1) + 2 * jacobsthal(x - 2));
+}
 
 int checkArg(const std::string& arg)
 {
@@ -43,36 +56,9 @@ int insertNbrVector(const std::string& arg, std::vector<int>& vector)
 	return (0);
 }
 
-int insertNbrDeque(const std::string& arg, std::deque<int>& deque)
-{
-	std::stringstream ss(arg);
-	std::string element;
-
-	while (std::getline(ss, element, ' '))
-	{
-		int nbr = atoi(element.c_str());
-		if (std::find(deque.begin(), deque.end(), nbr) != deque.end())
-		{
-			std::cerr << "Error: Duplicate number" << std::endl;
-			return (1);
-		}
-		deque.push_back(nbr);
-	}
-	return (0);
-}
-
 void displayVector(std::vector<int>& vector)
 {
 	for (std::vector<int>::iterator it = vector.begin(); it != vector.end(); it++)
-	{
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
-}
-
-void displayDeque(std::deque<int>& deque)
-{
-	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
@@ -101,6 +87,95 @@ int binarySearchVector(const std::vector<int> &S, int end, int key)
 	return (begin);
 }
 
+void insertElementsVector(std::vector<int> &S, const std::vector<std::pair<int, int> > &pairs, int last)
+{	
+	for (size_t i = 1; jacobsthal(i - 1) < pairs.size() - 1; i++)
+	{
+		size_t idx = jacobsthal(i);
+		size_t idx_prev = jacobsthal(i - 1);
+		while (idx > idx_prev)
+		{
+			if (idx < pairs.size())
+			{
+				int position = binarySearchVector(S, S.size() - 1, pairs[idx].first);
+				S.insert(S.begin() + position, pairs[idx].first);
+			}
+			idx--;
+		}
+	}
+	if (last != -1)
+	{
+		int position = binarySearchVector(S, S.size() - 1, last);
+		S.insert(S.begin() + position, last);
+	}
+}
+
+// https://en.wikipedia.org/wiki/Merge-insertion_sort
+
+int mergeInsertionSortVector(std::vector<int>& X)
+{
+	if (X.size() <= 1)
+		return (0);
+
+	std::vector<std::pair<int, int> > pairs;
+	std::vector<int> S;
+
+	// Group elements into pairs and determine the larger element in each pair and leave the last one if odd elements
+	for (size_t i = 0; i < X.size() / 2; ++i)
+	{
+		int a = X[2 * i];
+		int b = X[2 * i + 1];
+		if (a > b)
+			std::swap(a, b);
+		pairs.push_back(std::make_pair(a, b));
+		S.push_back(b);
+	}
+
+	// Recursively sort the larger elements from each pair
+	mergeInsertionSortVector(S);
+
+	// Insert at the start of S the element that was paired with the first and smallest element of S
+	S.insert(S.begin(), pairs[0].first);
+
+	// Insert the remaining elements into S
+	if (X.size() % 2 == 1)
+		insertElementsVector(S, pairs, X[X.size() - 1]);
+	else 
+		insertElementsVector(S, pairs, -1);
+
+	X = S;
+	return (0);
+}
+
+// ==============================================================================================
+
+int insertNbrDeque(const std::string& arg, std::deque<int>& deque)
+{
+	std::stringstream ss(arg);
+	std::string element;
+
+	while (std::getline(ss, element, ' '))
+	{
+		int nbr = atoi(element.c_str());
+		if (std::find(deque.begin(), deque.end(), nbr) != deque.end())
+		{
+			std::cerr << "Error: Duplicate number" << std::endl;
+			return (1);
+		}
+		deque.push_back(nbr);
+	}
+	return (0);
+}
+
+void displayDeque(std::deque<int>& deque)
+{
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
 int binarySearchDeque(const std::deque<int> &S, int end, int key)
 {
 	int begin = 0;
@@ -123,76 +198,29 @@ int binarySearchDeque(const std::deque<int> &S, int end, int key)
 	return (begin);
 }
 
-// Function to insert elements into the sorted sequence
-void insertElementsVector(std::vector<int> &S, const std::vector<std::pair<int, int> > &pairs, int last)
-{
-	for (size_t i = 1; i < pairs.size(); i++)
-	{
-		int position = binarySearchVector(S, S.size() - 1, pairs[i].first);
-		S.insert(S.begin() + position, pairs[i].first);
-	}
-	if (last != -1)
-	{
-		int position = binarySearchVector(S, S.size() - 1, last);
-		S.insert(S.begin() + position, last);
-	}
-		
-}
-
-// Function to insert elements into the sorted sequence
 void insertElementsDeque(std::deque<int> &S, const std::deque<std::pair<int, int> > &pairs, int last)
 {
-	for (size_t i = 1; i < pairs.size(); i++)
+	for (size_t i = 1; jacobsthal(i - 1) < pairs.size() - 1; i++)
 	{
-		int position = binarySearchDeque(S, S.size() - 1, pairs[i].first);
-		S.insert(S.begin() + position, pairs[i].first);
+		size_t idx = jacobsthal(i);
+		size_t idx_prev = jacobsthal(i - 1);
+		while (idx > idx_prev)
+		{
+			if (idx < pairs.size())
+			{
+				int position = binarySearchDeque(S, S.size() - 1, pairs[idx].first);
+				S.insert(S.begin() + position, pairs[idx].first);
+			}
+			idx--;
+		}
 	}
 	if (last != -1)
 	{
 		int position = binarySearchDeque(S, S.size() - 1, last);
 		S.insert(S.begin() + position, last);
 	}
-		
 }
 
-// Recursive function to sort the array using merge-insertion sort
-int mergeInsertionSortVector(std::vector<int>& X)
-{
-	if (X.size() <= 1)
-		return (0);
-
-	std::vector<std::pair<int, int> > pairs;
-	std::vector<int> S;
-
-	// Group elements into pairs and determine the larger element in each pair
-	for (size_t i = 0; i < X.size() / 2; ++i)
-	{
-		int a = X[2 * i];
-		int b = X[2 * i + 1];
-		if (a > b)
-			std::swap(a, b);
-		pairs.push_back(std::make_pair(a, b));
-		S.push_back(b);
-	}
-
-	// Recursively sort the larger elements (stored in S)
-	mergeInsertionSortVector(S);
-
-	// Insert the element that was paired with the first and smallest element of S
-	S.insert(S.begin(), pairs[0].first);
-
-	// Insert the remaining elements into S
-	if (X.size() % 2 == 1)
-		insertElementsVector(S, pairs, X[X.size() - 1]);
-	else 
-		insertElementsVector(S, pairs, -1);
-
-	// Copy the sorted sequence back to the original array
-	X = S;
-	return (0);
-}
-
-// Recursive function to sort the array using merge-insertion sort
 int mergeInsertionSortDeque(std::deque<int>& X)
 {
 	if (X.size() <= 1)
@@ -201,7 +229,6 @@ int mergeInsertionSortDeque(std::deque<int>& X)
 	std::deque<std::pair<int, int> > pairs;
 	std::deque<int> S;
 
-	// Group elements into pairs and determine the larger element in each pair
 	for (size_t i = 0; i < X.size() / 2; ++i)
 	{
 		int a = X[2 * i];
@@ -212,19 +239,15 @@ int mergeInsertionSortDeque(std::deque<int>& X)
 		S.push_back(b);
 	}
 
-	// Recursively sort the larger elements (stored in S)
 	mergeInsertionSortDeque(S);
 
-	// Insert the element that was paired with the first and smallest element of S
 	S.insert(S.begin(), pairs[0].first);
 
-	// Insert the remaining elements into S
 	if (X.size() % 2 == 1)
 		insertElementsDeque(S, pairs, X[X.size() - 1]);
 	else 
 		insertElementsDeque(S, pairs, -1);
 
-	// Copy the sorted sequence back to the original array
 	X = S;
 	return (0);
 }
